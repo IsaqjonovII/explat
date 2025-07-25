319a09e70439b42568fde466f6c9a0aa2f0d88b9
KhakimDon <almamedovkh@mail.ru>
2025-07-25 11:26:53 +0500

    fix: Error 25.07

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import eslint from "vite-plugin-eslint";
-
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(), eslint({
-            failOnError: true,
-        })
-    ],
-    server: {
-        port: 3000,
+  plugins: [
+    vue(),
+    eslint({
+      failOnError: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
-    },
-})
+  },
+  base: "./",
+});
